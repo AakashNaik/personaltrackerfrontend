fef911827b8aca95a440f76a1f85c94ae6cafd1c
Aakash Naik <aakashnaik150798@gmail.com>
2022-12-14 18:12:17 +0530

    added rest api/fetch from backend node server

--- src/components/SeeTasks.js
+++ src/components/SeeTasks.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 export default function SeeTasks()
 {
     const [msg,setmsg] = useState('Not called');
-    useEffect(()=>{fetch('http://localhost:4001/api/getAll').then(res=>{console.log(res);return res.json();}).then(data => {console.log(data);setmsg(data.output)})},[]);
+    useEffect(()=>{fetch('http://localhost:4001/api/getAll').then(res=>{return res.json();}).then(data => {console.log(data);setmsg(data.output)})},[]);
     return(
         <>
          <div>{msg}</div>
